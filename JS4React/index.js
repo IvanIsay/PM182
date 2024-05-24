42b7980e074e40a0685a6fc77b6b81973497377a
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-05-24 15:33:06 -0600

    pract3

--- JS4React/index.js
+++ JS4React/index.js
@@ -47,7 +47,6 @@ const usuario={
 console.log(usuario.enviaCorreo())
 
 
-*/
 
 // Trabajo com el DOM
 
@@ -64,3 +63,79 @@ boton.addEventListener('click',function (){
     titulo.innerText="Evento click ejecutado"
     alert('Exito')
 })
+
+
+*/
+
+// Trabajo con Arreglos
+
+const nombres= ['Alan', 'Isa', 'Pablo', 'Juan Luis']
+/*
+for(let i = 0 ; i< nombres.length; i++){
+    const elemento= nombres[i]
+    console.log(elemento)
+
+}
+
+nombres.forEach(function (nombre){
+    console.log(nombre)
+})
+
+// FUNCION MAP: Permite generar copia de un Array
+const array2= nombres.map(function (nombre){
+console.log(nombre)
+return nombre
+})
+
+console.log(nombres)
+console.log(array2)
+
+
+
+// FUNCION FIND: Ubicar elementos en un arreglo 
+const resultado = nombres.find(function (nombre){
+    if(nombre === 'Pablo'){
+        return nombre
+    }
+    })
+
+console.log(resultado)
+
+
+// FUNCION Filter: recorre el arreglo filtrado resultado en otro 
+const resultado = nombres.filter(function (nombre){
+    if(nombre != 'Pablo'){
+        return nombre
+    }
+    })
+
+console.log(nombres)    
+console.log(resultado)
+
+
+
+// FUNCION Concat: 
+const edades=[78,89,12,8,10,15]
+console.log(nombres.concat(edades))
+
+//Spread Operator
+console.log([...nombres, ...edades])
+
+*/
+
+// MODULOS: IMPORT & EXPORT
+
+//import {suma,resta} from "./calculadora.js"
+import * as calc from "./calculadora.js"
+
+
+
+console.log(calc.suma(45,54))
+console.log(calc.resta(45,5))
+
+
+
+
+
+
+
