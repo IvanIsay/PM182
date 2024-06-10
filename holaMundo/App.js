6385d900f2f8934a16d5693aa6c36ff793bc87d3
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-06-10 16:32:13 -0600

    pract5

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,26 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+
 
 export default function App() {
   return (
 
     <View style={styles.container}>
 
-      <Text> Hola mundo React Native :)  </Text>
 
+      <Text>   Hola mundo!  </Text>
+      <Button title='Presioname' color="#000000"></Button>
+
+      
       <StatusBar style="auto" />
-
     </View>
-
 
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+
 });
