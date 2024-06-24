85acebf8f982fcdfbbeb87ace89a90846f1cc2cd
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-06-24 15:42:32 -0600

    flatList

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,88 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button ,TextInput,ScrollView, Dimensions} from 'react-native';
-import React,{useState} from 'react';
-
-
+import { StyleSheet, Text, View,FlatList} from 'react-native';
+//import React,{useState} from 'react';
 
 export default function App() {
 
-  const[text,setText]= useState('valor Default')
-  const[submit,setSubmit]= useState('')
-
   return (
-
 
     <View style={styles.container}>
 
-      <ScrollView style={styles.scrollView}>
+      <FlatList  
+      data={[{key:1,name:'Ivan Isay'},
+             {key:2,name:'Victor'},
+             {key:3,name:'Elias'},
+             {key:4,name:'Alan'},
+             {key:5,name:'Pablo'},
+             {key:6,name:'Lilian'},
+             {key:7,name:'Monse'},
+             {key:8,name:'Monse'},
+             {key:9,name:'Monse'},
+             {key:10,name:'Monse'},
+             {key:11,name:'Monse'},
+             {key:12,name:'Monse'},
+             {key:13,name:'Monse'},
+             {key:14,name:'Monse'},
+             {key:15,name:'Monse'},
+             {key:16,name:'Monse'},
+             {key:17,name:'Monse'},
+             {key:18,name:'Monse'},
+             {key:19,name:'Monse'},
+             {key:20,name:'Monse'},
+             {key:21,name:'Monse'},
+             {key:22,name:'Monse'},
+             {key:23,name:'Monse'},
+             {key:24,name:'Monse'},
+      ]}
+      renderItem={ ({item})=><Text style= {styles.item}> {item.name} </Text>  }
+      />
 
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-      <Text> Componente: {submit}   </Text>
-
-
-      <TextInput style= {styles.input} placeholder='Escribe texto...'  onChangeText= { t=>setText(t) } defaultValue={text}   / >
-      
-      <Button title='Presioname...'  onPress={ ()=>{ setSubmit(text);  alert('Texto enviado')} }/>
-
-      </ScrollView>
-
+    
 
       <StatusBar style="auto" />
     </View>
@@ -91,23 +46,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     flexDirection:'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'strech',
     justifyContent: 'center',
+    paddingTop:60,
   },
-  input:{
-    /* backgroundColor:'#dfd7fd', */
-    width:'80%',
-    height:40,
-    borderBottomWidth: 1,
-    borderBottomColor:'blue',
-  },
-  scrollView:{
-    width:Dimensions.get('window').width
+  item:{
+    padding:10,
+    fontSize:24,
+    height:50,
+    borderColor:'blue',
+    borderBottomWidth:2
   }
 
 });
