492dba4bc45f1d4b3e8bf9e1ef9e8f0d8143d828
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-06-26 17:04:10 -0600

    consultar a API

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,40 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,FlatList,SectionList} from 'react-native';
-//import React,{useState} from 'react';
+import { StyleSheet, Text, View,FlatList,ActivityIndicator} from 'react-native';
+import React,{useState,useEffect} from 'react';
 
 export default function App() {
+
+  const [user,setUser]= useState([])
+  const [loading,setLoading]= useState(true)
+
+  useEffect(()=>{
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(data=>{setUser(data), setLoading(false)})
+  },[])
+
+
+  if(loading){
+    return <View style={styles.center}>
+         <ActivityIndicator size='large' color='#0000ff' /> 
+          <Text> Cargando </Text>
+    </View>
+  }
 
 
   return (
 
     <View style={styles.container}>
 
-      <SectionList
-         sections={[
-          { title:'Grupo A',
-            data:[
-              {key:1,name:'Ivan Isay'},
-              {key:2,name:'Victor'},
-              {key:3,name:'Elias'},
-              {key:4,name:'Alan'}, ]
-            },
-          { title:'Grupo B',
-              data:[  
-                {key:5,name:'Pablo'},
-                {key:6,name:'Lilian'},
-                {key:7,name:'Monse'},
-               ]
-            },
-          { title:'Grupo C',
-              data:[
-                {key:8,name:'Ivan'},
-                {key:9,name:'Vic'},
-                {key:10,name:'Eli'},
-                {key:11,name:'Alan'}, ]
-              },
-        ]}
-        renderItem={ ({item})=> <Text style={styles.item} > {item.name} </Text>  }
-        renderSectionHeader={ ({section})=> <Text style={styles.section} >{section.title}</Text>  }
+      <FlatList
+      data={user}
+      renderItem={({item})=> <Text style={styles.item}> {item.username}  {item.address.city}  </Text> }
       />
 
       <StatusBar style="auto" />
@@ -59,12 +53,11 @@ const styles = StyleSheet.create({
     borderColor:'blue',
     borderBottomWidth:2
   },
-  section:{
-    fontSize:16,
-    fontWeight:'bold',
-    backgroundColor:'#eee',
-    paddingTop:4,
-    paddingBottom:4,
+  center:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+
   }
 
 });
